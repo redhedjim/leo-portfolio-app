9f9620937c69bf8810e1e05b6525a937ce81e132
Leo <jamesleotucker@gmail.com>
2017-04-08 14:29:06 -0600

    update contact section

--- src/pages/mainPage/components/contactSection.js
+++ src/pages/mainPage/components/contactSection.js
@@ -6,30 +6,16 @@ const ContactSection = () => {
     width: "100%"   
   }
   return (
-    <div id="contact-top" className="contact-container">
-      <h2>Contact me</h2>
-      <form action="" className="contact-form form">
-        <TextFieldGroup
-          id={"name"}
-          placeholder={"What should I call you?"}
-        />
-        <TextFieldGroup
-          id={"email"}
-          type={"email"}
-          placeholder={"How should I reach you?"}
-        />
-        <TextFieldGroup
-          id={"animal"}
-          placeholder={"What is your favourite animal? (Optional, but encouraged)"}
-        />
-        <div className="form-group col-xs-12">
-          <textarea placeholder="What would you like to say?" id="message" rows="5" style={styles}></textarea>
+      <div className="contact-container">
+        <div id="contact-header">        
+          <h2>Contact me</h2>
         </div>
-        <div className="form-group col-xs-12">
-          <button className="btn btn-primary">Send away</button>
+        <div id="contact-body">
+          <div className="social-media"><a href="http://www.github.com/redhedjim"><img className="img-responsive social-media-icon" src="https://image.flaticon.com/icons/png/512/25/25231.png"/></a></div>
+          <div className="social-media"><a href="mailto:jamesleotucker@gmail.com"><img className="img-responsive social-media-icon" src="http://www.stickpng.com/assets/images/584856b4e0bb315b0f7675ac.png"/></a></div>
+          <div className="social-media"><a href="https://www.linkedin.com/in/leo-tucker-738a4433/"><img className="img-responsive social-media-icon" src="http://findicons.com/files/icons/2779/simple_icons/512/linkedin_512_black.png"/></a></div>
         </div>
-      </form>
-    </div>
+      </div>
   );
 };
 
